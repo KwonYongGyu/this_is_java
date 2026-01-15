e6101093c336bd4c5932eef88b5e770697f480a6
KwonYongGyu <kyg020726@gmail.com>
2026-01-15 11:23:01 +0900

    ch01 종료 패키지는 src에 new 패키지로 생성하고 java파일을 생성후 드래그하거나 잘라내기로 패키지 밑으로 파일을 Refactor가 가능하다

---
+++ chapter01/src/guguDan.java
@@ -0,0 +1,39 @@
+public class guguDan {
+    public static void main(String[] args) {
+        int i, k;
+
+        for (i = 2; i <= 9; i++) {
+
+            System.out.println(i + "단 출력");
+
+            for (k = 1; k <= 9; k++) {
+
+                System.out.println(i + " " + "*" + " " + k + " " + "=" + " " + (i * k));
+
+            }
+
+
+        }
+
+        for (int digit = 2; digit < 9; digit += 3) {
+            for (int n = 0; n < 10; n++) {
+                System.out.println(
+                        (
+                                (n == 0)
+                                        ? String.format("%d단 출력       %d단 출력       %d단 출력", digit, digit + 1, digit + 2)
+                                        : ((digit < 8) ? String.format("%d * %d = %d     %d * %d = %d     %d * %d = %d"
+                                        , digit, n, digit * n
+                                        , digit + 1, n, (digit + 1) * n
+                                        , digit + 2, n, (digit + 2) * n)
+                                        : String.format("%d * %d = %d     %d * %d = %d"
+                                        , digit, n, digit * n
+                                        , digit + 1, n, (digit + 1) * n))
+                        )
+                );
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
+
