31519e15206885113b077d56d0f8dd9cb67e04cc
KwonYongGyu <kyg020726@gmail.com>
2026-01-20 14:55:04 +0900

    p.165꺄지의 예제들 contains는 true,false를 리턴하고 indexOf는 포함되어있지 않으면 -1을 포함시 인덱스 번호0,1,2...를 리턴한다. substring(0, 6)이면 0부터 6앞까지 즉, 0부터 5까지이다. 문자의 리터럴이 동일하다면 String 객체를 공유한다. 즉 참조와 문자열이 같다. new연산자로 직접 String 객체를 생성하고 대입시 새로운 객체를 생성했기때문에 문자열은 같지만 참조는 다르다.

--- chapter05/src/main/java/com/mjc813/Chapter05.java
+++ chapter05/src/main/java/com/mjc813/Chapter05.java
@@ -32,8 +32,123 @@ public class Chapter05 {
         hobby = null;   // "여행"에 해당하는 String 객체를 쓰레기로 만듦
 
         String kind1 = "자동차";
+
         String kind2 = kind1;   //kind1 변수에 저장되어 있는 번지를 kind2변수에 대입
         kind1 = null;    //"자동차"에 해당하는 String 객체 쓰레기가 아님
         System.out.println("kind2: " + kind2);
+    }
+
+    // p.158
+    public void EqualsExample() {
+        String strVar1 = "홍길동";
+        String strVar2 = "홍길동";
+        // 자바는 문자열 리터럴이 동일하다면 String 객체를 공유하도록 설계되어 있다.
+        // "홍길동"으로 같은 리터럴을 공유하기에 strVar1, strVar2는 객체를 공유한다.
+
+        if (strVar1 == strVar2) {
+            System.out.println("strVar1과 strVar2는 참조가 같음");
+        } else {
+            System.out.println("strVar1과 strVar2는 참조가 다름");
+        }
+
+        if (strVar1.equals(strVar2)) {
+            System.out.println("strVar1과 strVar2는 문자열일 같음");
+        }
+        String strVar3 = new String("홍길동");
+        String strVar4 = new String("홍길동");
+        // "홍길동"으로 같은 리터럴을 가지지만 둘을 다른 주소를 가진다.
+        // new 연산자로 직접 String 객체를 생성하고 대입해서 둘은 서로 다른 객체를 가진다.
+
+        if (strVar3 == strVar4) {
+            System.out.println("strVar3과 strVar4는 참조가 같음");
+        } else {
+            System.out.println("strVar3과 strVar4는 참조가 다름");
+        }
+
+        if (strVar3.equals(strVar4)) {
+            System.out.println("strVar3과 strVar4는 문자열이 같음");
+        }
+    }
+
+    // p.159
+    public void EmptyStringExample() {
+        String hobby = "";
+        if (hobby.equals("")) {
+            System.out.println("hobby 변수가 참조하는 String 객체는 빈 문자열");
+        }
+    }
+
+    // p.160
+    public void CharAtExample() {
+        String ssn = "95062410123";
+        char sex = ssn.charAt(6);
+        switch (sex) {
+            case '1':
+            case '3':
+                System.out.println("남자입니다.");
+                break;
+            case '2':
+            case '4':
+                System.out.println("여자입니다.");
+                break;
+        }
+    }
+
+    // p.161
+    public void LengthExample() {
+        String ssn = "9506241230123";
+        int length = ssn.length();
+        if (length == 13) {
+            System.out.println("주민등록번호 자릿수가 맞습니다.");
+        } else {
+            System.out.println("주민등록번호 자릿수가 틀립니다.");
+        }
+    }
+
+    // p.162
+    public void ReplaceExample() {
+        String oldStr = "자바 문자열은 불변입니다. 자바 문자열은 String입니다.";
+        String newStr = oldStr.replace("자바", "JAVA");
+    // 자바에서 문자열은 불변하므로 .replace를 이용해 완전히 새로운 문자열 객체를 반환
+        System.out.println(oldStr); // 원본 문자열
+        System.out.println(newStr); // 새로운 문자열 객체
+    }
+
+    // p.163
+    public void SubStringExample() {
+        String ssn = "880815-1234567";
+
+        String firstNum = ssn.substring(0, 6);
+        System.out.println(firstNum);
+
+        String secondNum = ssn.substring(7);
+        System.out.println(secondNum);
+    }
+
+    // p.165
+    public void IndexOfContainsExample() {
+        String subject = "자바 프로그래밍";
+    // indexOf 방식은 리턴된 숫자값이 -1이 아닐때 (0,1,2.... 등 위치값이 나올 때)
+        int location = subject.indexOf("프로그래밍");
+        // 정수를 리턴하기에 location을 정수가 아닌 다른 값으로 설정하면 오류발생
+        System.out.println(location); // 인덱스 위치 3
+        String substring = subject.substring(location);
+        System.out.println(substring);
+
+        location = subject.indexOf("자바");   // 인덱스 0번 리턴
+        //자바랑 관련이 없으면 -1 리턴
+        if (location != -1) { // -1 이 아니면 즉 관련이 있으면
+            System.out.println("자바와 관련된 책이군요.");
+        } else {
+            System.out.println("자바와 관련 없는 책이군요");
+        }
+
+        // contains 방식은 리턴된 결과값이 true일 때
+        boolean result = subject.contains("자바"); // subject에 자바가 포함되어 있는가?
+        if (result) {
+            System.out.println("자바와 관련된 책이군요");
+        } else {
+            System.out.println("자바와 관련 없는 책이군요");
+        }
     }
 }
--- chapter05/src/main/java/com/mjc813/Main.java
+++ chapter05/src/main/java/com/mjc813/Main.java
@@ -3,7 +3,7 @@ package com.mjc813;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[]args) {
+    public static void main(String[] args) {
 //        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
 //        // to see how IntelliJ IDEA suggests fixing it.
 //        IO.println(String.format("Hello and welcome!"));
@@ -12,10 +12,16 @@ public class Main {
 //            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
 //            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
 //            IO.println("i = " + i);
-            Chapter05 ct05 = new Chapter05();
-            ct05.ReferenceVariableCompareExample();
-            ct05.NullPointerExceptionExample();
-            ct05.GarbageObjectExample();
-
+        Chapter05 ct05 = new Chapter05();
+        ct05.ReferenceVariableCompareExample();
+        ct05.NullPointerExceptionExample();
+        ct05.GarbageObjectExample();
+        ct05.EqualsExample();
+        ct05.EmptyStringExample();
+        ct05.CharAtExample();
+        ct05.LengthExample();
+        ct05.ReplaceExample();
+        ct05.SubStringExample();
+        ct05.IndexOfContainsExample();
     }
 }
