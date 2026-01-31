d3f8eafbca3e67ebf6e3182a15545d60327238a3
권용규 <kyg020726@gmail.com>
2026-02-01 01:50:22 +0900

    p.375

--- chapter08/src/main/java/com.mjc813/Chapter08.java
+++ chapter08/src/main/java/com.mjc813/Chapter08.java
@@ -111,6 +111,20 @@ public class Chapter08 {
         a = e;  // A <- E 자동 타입 변환
 
     }
+    // p.375
+    public void CastingExample(){
+        // 인터페이스 변수 선언과 구현 객체 대입
+        Vehicle vehicle = new Bus();
+
+        // 인터페이스를 통해서 호출
+         vehicle.run();
+         // vehicle.checkFare(); (x)
+
+        // 강제 타입 변환 후 호출
+        Bus bus = (Bus) vehicle;
+        bus.run();
+        bus.checkFare();
+    }
 
 
     // 확인문제 5.
---
+++ chapter08/src/main/java/com.mjc813/Vehicle.java
@@ -0,0 +1,6 @@
+package com.mjc813;
+
+public interface Vehicle {
+    // 추상 메소드
+    void run();
+}
