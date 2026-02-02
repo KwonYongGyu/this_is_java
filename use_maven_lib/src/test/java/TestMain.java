f8f1cd6408c33d07bc770257751dac082c9f0850
KwonYongGyu <kyg020726@gmail.com>
2026-02-02 15:42:56 +0900

    maven에서 단위테스트 3개 완료 예상값과 실제값 확인

--- use_maven_lib/src/test/java/TestMain.java
+++ use_maven_lib/src/test/java/TestMain.java
@@ -1,3 +1,5 @@
+import com.google.gson.Gson;
+import com.mjc813.MyClass;
 import org.testng.annotations.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -13,5 +15,17 @@ public class TestMain {
     @Test
     public void testMain2(){
         assertThat("aaa").isEqualTo("a"+"aa").isAlphanumeric();
+    }
+
+    @Test
+    public void testGson(){
+        Gson gs = new Gson();
+        MyClass mc = new MyClass();
+        String s = gs.toJson(mc);
+
+        MyClass mc2 = gs.fromJson(s, MyClass.class);
+        assertThat(mc2.getMyArray().length).isEqualTo(mc.getMyArray().length);
+        assertThat(mc2.getMyArray()).isEqualTo(mc.getMyArray());
+
     }
 }
