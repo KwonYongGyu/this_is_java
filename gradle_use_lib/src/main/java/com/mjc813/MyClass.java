1cfd7bce7448652e0d07c405714420cc7a67061c
KwonYongGyu <kyg020726@gmail.com>
2026-02-02 16:09:41 +0900

    radle 을 이용하여 Gson 라이브러리 사용하기

---
+++ gradle_use_lib/src/main/java/com/mjc813/MyClass.java
@@ -0,0 +1,13 @@
+package com.mjc813;
+
+public class MyClass {
+    private int[] myArray = new int[]{1, 2, 3, 4, 5, 6};
+
+    public void setMyArray(int[] arr) {
+        this.myArray = arr;
+    }
+
+    public int[] getMyArray() {
+        return this.myArray;
+    }
+}
--- gradle_use_lib/src/test/java/com/mjc813/TestMyClass.java
+++ gradle_use_lib/src/test/java/com/mjc813/TestMyClass.java
@@ -1,11 +1,11 @@
+package com.mjc813;
+
 import com.google.gson.Gson;
-import com.mjc813.MyClass;
 import org.testng.annotations.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.testng.AssertJUnit.assertEquals;
 
-public class TestMain {
+public class TestMyClass {
     @Test
     public void TestMain1(){
         assertThat(2+1).isEqualTo(3);
@@ -21,8 +21,8 @@ public class TestMain {
     public void testGson(){
         Gson gs = new Gson();
         MyClass mc = new MyClass();
-        String s = gs.toJson(mc);
 
+        String s = gs.toJson(mc);
         MyClass mc2 = gs.fromJson(s, MyClass.class);
         assertThat(mc2.getMyArray().length).isEqualTo(mc.getMyArray().length);
         assertThat(mc2.getMyArray()).isEqualTo(mc.getMyArray());
