80de97a36d2ae9c35f8b1f9aabe4fea891c711a0
KwonYongGyu <kyg020726@gmail.com>
2026-02-03 16:12:03 +0900

    계산기.length로 수정

--- chapter10/mjc813_lib/src/main/java/com/mjc813/machine/Calculator.java
+++ chapter10/mjc813_lib/src/main/java/com/mjc813/machine/Calculator.java
@@ -3,8 +3,8 @@ package com.mjc813.machine;
 public class Calculator {
     public long add(int ... items) {
         long sum = 0L;
-        for (int item : items) {
-            sum += item;
+        for(int i = 0; i<items.length; i++){
+            sum += items[i];
         }
         return sum;
     }
