30ad954b6620433b8dcaf9bdd8d67603230dae60
권용규 <kyg020726@gmail.com>
2026-02-05 03:00:29 +0900

    잘못 커밋해서 덮어쓰기

--- Chapter11/src/main/java/com/mjc813/banking/IMachine.java
+++
@@ -1,5 +0,0 @@
-package com.mjc813.banking;
-
-public interface IMachine {
-	boolean isActive() throws MachineNotWorkingException;
-}
--- Chapter11/src/main/java/com/mjc813/banking/MachineNotWorkingException.java
+++
@@ -1,7 +0,0 @@
-package com.mjc813.banking;
-
-public class MachineNotWorkingException extends Exception {
-	public MachineNotWorkingException(String msg) {
-		super(msg);
-	}
-}
