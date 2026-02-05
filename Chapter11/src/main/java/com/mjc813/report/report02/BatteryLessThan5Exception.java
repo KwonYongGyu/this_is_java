b25918375db6b8466354eca7e0405f5c9ccf06dd
KwonYongGyu <kyg020726@gmail.com>
2026-02-05 16:27:14 +0900

    report2 배터리가 5% 미만인 예외를 만들어 던지기 예외 이름은 BatteryLessThan5Exception    + game1 소프트웨어가 없다 라는 예외를 만들어서 던지기 얘외 클래스 이름은 Game1IsNullException

--- Chapter11/src/main/java/com/mjc813/report/report02/BatteryLessThan5Exception.java
+++ Chapter11/src/main/java/com/mjc813/report/report02/BatteryLessThan5Exception.java
@@ -1,4 +1,6 @@
 package com.mjc813.report.report02;
 
-public class BatteryLessThan5Exception {
+public class BatteryLessThan5Exception extends Exception{
+    public BatteryLessThan5Exception(int battery, String s) {
+    }
 }
--- Chapter11/src/main/java/com/mjc813/report/report02/Game1IsNullException.java
+++ Chapter11/src/main/java/com/mjc813/report/report02/Game1IsNullException.java
@@ -1,4 +1,6 @@
 package com.mjc813.report.report02;
 
-public class Game1IsNullException {
+public class Game1IsNullException extends Exception{
+    public Game1IsNullException(String gameIsNull, String game1SoftwareIsNull) {
+    }
 }
