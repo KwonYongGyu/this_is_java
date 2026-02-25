d2023ec5429f1d66e58343492338e1d40b1d3e5a
KwonYongGyu <kyg020726@gmail.com>
2026-02-25 17:21:29 +0900

    p.661까지 완료

---
+++ chapter15/src/main/java/com/mjc813/bookExample/Chapter15.java
@@ -0,0 +1,124 @@
+package com.mjc813.bookExample;
+
+
+import java.util.*;
+import java.util.Map.Entry;
+
+public class Chapter15 {
+    public void ArrayListExample(){
+        //ArrayList 컬랙션 생성
+        List<Board> list = new ArrayList<>();
+
+        // 객체 추가
+        list.add(new Board("제목1", "내용1", "글쓴이1"));
+        list.add(new Board("제목2", "내용2", "글쓴이2"));
+        list.add(new Board("제목3", "내용3", "글쓴이3"));
+        list.add(new Board("제목4", "내용4", "글쓴이4"));
+        list.add(new Board("제목5", "내용5", "글쓴이5"));
+
+        // 저장된 총 객체 수 얻기
+        int size = list.size();
+        System.out.println("총 객체 수: " + size);
+        System.out.println();
+
+        // 특정 인텍스의 객체 가져오기
+        Board board = list.get(2);
+        System.out.println(board.getSubject() + "\t" + board.getContent()
+                            + "\t" + board.getWriter());
+        System.out.println();
+
+        // 모든 객체를 하나씩 가져오기
+        for(int i =0; i< list.size(); i++) {
+            Board b = list.get(i);
+            System.out.println(b.getSubject() + "\t" + b.getContent()
+                                + "\t" + b.getWriter());
+        }
+        System.out.println();
+
+        // 객체 삭제
+        list.remove(2);
+        list.remove(2);
+
+        // 향상된 for 문으로 모든 객체를 하나씩 가져오기
+        for(Board b : list) {
+            System.out.println(b.getSubject() + "\t" + b.getContent()
+                                + "\t" + b.getWriter());
+        }
+        System.out.println();
+    }
+    public void LinkedListExample(){
+        // ArrayList 컬렉션 객체 생성
+        List<String> list1 = new ArrayList<String>();
+
+        // LinkedList 컬렉션 객체 생성
+        List<String> list2 = new LinkedList<String>();
+
+        // 시작 시간과 끝 시간을 저장할 변수 선언
+        long startTime;
+        long endTime;
+
+        // ArrayList 컬렉션에 저장하는 시간 측정
+        startTime = System.nanoTime();
+        for(int i = 0; i < 10000; i++) {
+            list1.add(0, String.valueOf(i));
+        }
+        endTime = System.nanoTime();
+        System.out.printf("%-17s %8d ns \n", "ArrayList 걸린 시간: ",
+                (endTime - startTime) );
+
+        // LinkedList 컬렉션에 저장하는 시간 측정
+        startTime = System.nanoTime();
+        for(int i = 0; i < 10000; i++) {
+            list2.add(0, String.valueOf(i));
+        }
+        endTime = System.nanoTime();
+        System.out.printf("%-17s %8d ns \n", "LinkedList 걸린 시간: ",
+                (endTime - startTime) );
+        System.out.println();
+    }
+
+    public void HashMapExample() {
+        // Map 컬렉션 생성
+        Map<String, Integer> map = new HashMap<>();
+
+        // 객체 저장
+        map.put("신용권", 85);
+        map.put("홍길동", 90);
+        map.put("동장군", 80);
+        map.put("홍길동", 95); // 키가 같기때문에 제읾 마지막에 저장한 값만 저장
+        System.out.println("총 Entry 수: " + map.size());
+        System.out.println();
+
+        // 키로 값 얻기
+        String key = "홍길동";
+        int value = map.get(key);   // 키를 매개값으로 주면 값을 리턴
+        System.out.println(key + ": " + value);
+        System.out.println();
+
+        // 키 Set 컬렉션 얻고, 반복해서 키와 값을 얻기
+        Set<String> keySet = map.keySet();
+        Iterator<String> keyIterator = keySet.iterator();
+        while (keyIterator.hasNext()) {
+            String k = keyIterator.next();
+            Integer v = map.get(k);
+            System.out.println(k + ": " + v);
+        }
+        System.out.println();
+
+        // 엔트리 Set 컬렉션을 얻고, 반복해서 키와 값을 얻기
+        Set<Entry<String, Integer>> entrySet = map.entrySet();
+        Iterator<Entry<String, Integer>> entryIterator = entrySet.iterator();
+        while (entryIterator.hasNext()) {
+            Entry<String, Integer> entry = entryIterator.next();
+            String k =entry.getKey();
+            Integer v = entry.getValue();
+            System.out.println();
+        }
+        System.out.println();
+
+        // 키로 엔트리 삭제
+        map.remove("홍길동");
+        System.out.println("총 Entry 수: " + map.size());
+        System.out.println();
+    }
+}
---
+++ chapter15/src/main/java/com/mjc813/bookExample/Main.java
@@ -0,0 +1,12 @@
+package com.mjc813.bookExample;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Chapter15 ch15 = new Chapter15();
+        ch15.ArrayListExample();
+        ch15.LinkedListExample();
+        ch15.HashMapExample();
+    }
+}
