e75722b7d918fa4623f774a6e9efd3b87646830b
KwonYongGyu <kyg020726@gmail.com>
2026-03-10 20:57:36 +0900

    while안에 try가 들어가면 예외가 발생해도 다음을 기다릴 수 있다.

--- chapter19/src/main/java/com/mjc813/Main.java
+++ chapter19/src/main/java/com/mjc813/Main.java
@@ -1,12 +1,12 @@
-package com.mjc813;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        Chapter19 ch19 = new Chapter19();
-        ch19.inetAddressExample();
-
-//        ch19.ClientExample();
-    }
-}
+//package com.mjc813;
+//
+////TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//public class Main {
+//    public static void main(String[] args) {
+//        Chapter19 ch19 = new Chapter19();
+////        ch19.inetAddressExample();
+//
+////        ch19.ClientExample();
+//    }
+//}
--- chapter19/src/main/java/com/mjc813/ServerExample.java
+++ chapter19/src/main/java/com/mjc813/ServerExample.java
@@ -1,6 +1,9 @@
 package com.mjc813;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -33,32 +36,56 @@ public class ServerExample {
     }
 
     public static void startServer() {
+
         // 작업 스레드 정의
-        Thread thread = new Thread() {
+         Thread thread = new Thread() {
             @Override
             public void run() {
+                Socket socket = null;
+                BufferedReader breader = null;
+                InetSocketAddress isa = null;
                 try {
                     // ServerSocket 생성 및 Port 바인딩
-                    serverSocket = new ServerSocket(50001);
+                    serverSocket = new ServerSocket();
+                    serverSocket.bind(new InetSocketAddress(50001));
+
+                    // serverSocket = new ServerSocket(50001);
                     System.out.println("[서버] 시작됨");
 
                     while(true) {
                         System.out.println("\n [서버] 연결 요청을 기다림\n");
                         // 연결 수락
-                        Socket socket = serverSocket.accept();
+                        socket = serverSocket.accept();
 
                         // 연결된 클라이언트 정보 읽기
-                        InetSocketAddress isa =
-                                (InetSocketAddress) socket.getRemoteSocketAddress();
+                        isa = (InetSocketAddress) socket.getRemoteSocketAddress();
                         System.out.println("[서버] " + isa.getHostName() + "의 연결 요청을 수락함");
 
                         // 연결 끊기
+//                        socket.close();
+//                        System.out.println("[서버] " + isa.getHostName() + "의 연결을 끊음");
+                         breader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        String msg = breader.readLine();
+                        System.out.println(msg);
                         socket.close();
-                        System.out.println("[서버] " + isa.getHostName() + "의 연결을 끊음");
+
+
                     }
                 } catch (IOException e) {
                     System.out.println("[서버] " + e.getMessage());
+                } finally {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                    }
+                    try {
+                        breader.close();
+                    } catch (IOException e) {
+                    }
                 }
+                // 연결 끊기
+                System.out.println("[서버] " + isa.getHostName() + "의 연결을 끊음");
+
             }
         };
         // 스레드 시작
