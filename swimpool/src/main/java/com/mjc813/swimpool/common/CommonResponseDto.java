02b59bce7c4cb0f8ec6b0e3704f5abc956d00977
KwonYongGyu <kyg020726@gmail.com>
2026-05-07 21:46:39 +0900

    swimpool프로젝트 새로 생성, AWS 계정 생성, FileZillar, PuTTy 설치

---
+++ swimpool/src/main/java/com/mjc813/swimpool/common/CommonResponseDto.java
@@ -0,0 +1,16 @@
+package com.mjc813.swimpool.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CommonResponseDto<T> {
+	private CommonResponseCode code;
+	private String message;
+	private T result;
+
+	public static <T> CommonResponseDto<T> make(CommonResponseCode code, T data) {
+		return new CommonResponseDto<T>(code, code.getMessage(), data);
+	}
+}
