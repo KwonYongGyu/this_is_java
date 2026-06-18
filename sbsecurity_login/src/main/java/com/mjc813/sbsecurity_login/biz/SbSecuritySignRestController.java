aa8acb224f00000c128e387a13a81cfe9bfb8600
KwonYongGyu <kyg020726@gmail.com>
2026-06-18 19:20:07 +0900

    SessionSignRestController를 SbSecuritySignRestController로 이름 변경

--- sbsecurity_login/src/main/java/com/mjc813/sbsecurity_login/biz/SbSecuritySignRestController.java
+++ sbsecurity_login/src/main/java/com/mjc813/sbsecurity_login/biz/SbSecuritySignRestController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/auth")
-public class SessionSignRestController {
+public class SbSecuritySignRestController {
 	@Autowired
 	private MemberService memberService;
 	@Autowired
