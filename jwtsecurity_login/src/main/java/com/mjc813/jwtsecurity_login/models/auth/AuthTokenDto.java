7eb32053dd26379619b6612967db6dcdd8efe777
KwonYongGyu <kyg020726@gmail.com>
2026-06-19 21:23:40 +0900

    jwtsecurity_login 프로젝트에서 사인인 정상일때 accessToken, refreshToken 을 발급해서 응답

---
+++ jwtsecurity_login/src/main/java/com/mjc813/jwtsecurity_login/models/auth/AuthTokenDto.java
@@ -0,0 +1,10 @@
+package com.mjc813.jwtsecurity_login.models.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AuthTokenDto {
+    private String accessToken;
+}
